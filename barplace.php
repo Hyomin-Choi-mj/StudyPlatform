1349d269f13e23896eb598c96859f05d3ee0a2ea
Hyomin-Choi-mj <Hyomin-Choi-mj@users.noreply.github.com>
2021-01-24 00:58:27 +0900

    ㅇㄹ
    ㄹ

---
+++ barplace.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>술자리 지도</title>
+</head>
+<body>
+        <center><font size = 40>Bar Placement</font></center>
+</body>
+</html>
